2b0e190b05ca8c9c6ae4eda4aca2d62e4ff7a30f
qsniyg <qsniyg@mail.com>
2018-04-22 04:54:49 -0700

    Fix for new version

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ var inputel = document.getElementById("input");
 var maxael = document.getElementById("max_a");
 var maxspanel = document.getElementById("max_span");
 var maximgel = document.getElementById("max_img");
+var currenturl = null;
 inputel.oninput = function() {
   var text = inputel.value;
   if (text.match(/^https?:\/\//)) {
-    var newurl = imu_variable(text);
-    if (newurl !== text) {
-      set_max(newurl);
+    var newurl = imu_variable(text, {fill_object:true});
+    if (newurl.url instanceof Array) {
+      if (newurl.url.indexOf(text) >= 0) {
+        set_max(false);
+      } else {
+        set_max(newurl.url);
+      }
+    } else if (newurl.url !== text) {
+      set_max(newurl.url);
     } else {
       set_max(false);
     }
@@ -63,13 +70,13 @@ function proxify(url) {
   //return "https://imagesvc.timeincapp.com/v3/foundry/image/?url=" + encodeURIComponent(url);
 }
 
-function set_max(url) {
-  if (!url) {
-    if (url === false)
+function set_max(urls) {
+  if (!urls) {
+    if (urls === false)
       maxspanel.innerHTML = "No larger image found";
-    else if (url === undefined)
+    else if (urls === undefined)
       maxspanel.innerHTML = "Invalid URL";
-    else if (url === null)
+    else if (urls === null)
       maxspanel.innerHTML = "";
 
     maxael.innerHTML = "";
@@ -78,19 +85,45 @@ function set_max(url) {
     return;
   }
 
-  if (url === maxael.innerHTML)
+  if (JSON.stringify(urls) === JSON.stringify(currenturl))
     return;
 
-  console.log(url);
-  var proxyurl = proxify(url);
+  console.log(urls);
+  //var proxyurl = proxify(url);
+
+  maxael.innerHTML = "";
   maxspanel.innerHTML = "";
-  maxael.innerHTML = url;
-  maxael.href = url;
-  SelectText("max_a");
-  var successful = document.execCommand('copy');
-  unselect();
-  if (successful === true) {
-    maxspanel.innerHTML = "Copied to clipboard!";
+
+  if (!(urls instanceof Array)) {
+    urls = [urls];
+  }
+
+  for (var i = 0; i < urls.length; i++) {
+    var url = urls[i];
+
+    var suba = document.createElement("a");
+    suba.setAttribute("rel", "noreferrer");
+    suba.href = url;
+    suba.innerHTML = url;
+    var subp = document.createElement("p");
+    subp.innerHTML = "OR";
+    maxael.appendChild(suba);
+
+    if ((i + 1) < urls.length)
+      maxael.appendChild(subp);
+  }
+  /*maxael.innerHTML = url;
+  maxael.href = url;*/
+
+  currenturl = urls;
+
+  if (urls.length === 1) {
+    SelectText("max_a");
+    var successful = document.execCommand('copy');
+    unselect();
+    if (successful === true) {
+      maxspanel.innerHTML = "Copied to clipboard!";
+    }
   }
   //maximgel.style.backgroundImage = "url('" + proxyurl + "')";
 }
