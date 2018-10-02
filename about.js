d1bf14737fb612078d95d943239593f176e8a2c4
qsniyg <qsniyg@mail.com>
2018-10-02 15:38:54 -0700

    Fix counting for amazon/google websites, improve option style

--- about.js
+++ about.js
@@ -17,15 +17,34 @@ function fuzzify(num) {
 
 function reqListener () {
   var response = this.responseText;
+  userscript_contents = response;
   response = response
     .replace(/^[\s\S]*function bigimage/, "")
     .replace(/\/\/ *-- *end *bigimage *--[\s\S]*$/, "");
   document.getElementById("rules").innerHTML = fuzzify(response.match(/\n        if /g).length);
   //document.getElementById("sites").innerHTML = fuzzify(response.match(/(?:domain[_ ]|[^(]domain\.)/g).length);
-  document.getElementById("sites").innerHTML = fuzzify(response.match(/domain[_ ]/g).length);
+  document.getElementById("sites").innerHTML = fuzzify(response.match(/(?:domain[_ ]|amazon_container|googlestorage_container)/g).length);
+}
+
+function get_sites() {
+  var sites = userscript_contents.match(/(?:domain(?:_[a-z]+)?|amazon_container|googlestorage_container) *=== *["'](.*?)["']/g);
+  var siteslist = [];
+  for (var i = 0; i < sites.length; i++) {
+    var site = sites[i].match(/["'](.*?)["']$/)[1].replace(/^www\./, "");
+    if (sites[i].match(/^amazon_/))
+      site = site + ".s3.amazonaws.com";
+    if (sites[i].match(/^googlestorage_/))
+      site = site + ".storage.googleapis.com";
+    if (siteslist.indexOf(site) < 0)
+      siteslist.push(site);
+  }
+  siteslist.sort();
+
+  return siteslist;
 }
 
 var userscript_location = "https://rawgit.com/qsniyg/maxurl/master/userscript.user.js";
+var userscript_contents = null;
 
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
