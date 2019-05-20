e35bdac50f41d5ad1b98a471cffd9cc887299047
qsniyg <qsniyg@mail.com>
2019-05-19 20:06:51 -0700

    Fix gallery for non-images (fixes #65)

--- userscript.user.js
+++ userscript.user.js
@@ -31621,8 +31621,26 @@ var $$IMU_EXPORT$$;
         function valid_source(source) {
             var thresh = 20;
 
+            if (source.tagName !== "PICTURE" &&
+                source.tagName !== "IMG" &&
+                source.tagName !== "SOURCE") {
+                var style = get_computed_style(source);
+                if (style.getPropertyValue("background-image")) {
+                    var bgimg = style.getPropertyValue("background-image");
+                    if (!bgimg.match(/^ *url[(]/)) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+
             return !(source.width && source.width < thresh ||
                      source.height && source.height < thresh);
+        }
+
+        function get_computed_style(el) {
+            return el.currentStyle || window.getComputedStyle(el);
         }
 
         function find_source(els) {
