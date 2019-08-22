9fe62460cb3a47fb4873f624479156fe6c75d4a5
qsniyg <qsniyg@mail.com>
2019-08-22 02:37:42 -0700

    Add progress bar

--- userscript.user.js
+++ userscript.user.js
@@ -41479,8 +41479,37 @@ if (domain_nosub === "lystit.com" && domain.match(/cdn[a-z]?\.lystit\.com/)) {
 
             var finish_img = function() {
                 finished++;
+                progressb_el.style.width = ((finished/imgs.length)*100) + "%";
                 console_log("Finished " + finished + "/" + imgs.length);
+
+                if (finished >= imgs.length) {
+                    progressc_el.parentElement.removeChild(progressc_el);
+                }
             };
+
+            var progressc_el = document.createElement("div");
+            progressc_el.style.all = "initial";
+            progressc_el.style.position = "absolute";
+            progressc_el.style.top = "0px";
+            progressc_el.style.left = "0px";
+            progressc_el.style.width = "80%";
+            progressc_el.style.marginTop = "100px";
+            progressc_el.style.marginLeft = "10%";
+            progressc_el.style.backgroundColor = "rgba(0,0,0,0.7)";
+            //progressc_el.style.padding = "1em";
+            progressc_el.style.height = "2em";
+
+            var progressb_el = document.createElement("div");
+            progressb_el.style.all = "initial";
+            progressb_el.style.position = "absolute";
+            progressb_el.style.top = "0px";
+            progressb_el.style.left = "0px";
+            progressb_el.style.backgroundColor = "#00aa00";
+            progressb_el.style.height = "100%";
+            progressb_el.style.width = "0%";
+
+            progressc_el.appendChild(progressb_el);
+            document.documentElement.appendChild(progressc_el);
 
             for (var i = 0; i < imgs.length; i++) {
                 var source = find_source([imgs[i]]);
