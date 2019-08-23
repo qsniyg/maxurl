65379e98fd220b52936fdeb3773023a1a6165372
qsniyg <qsniyg@mail.com>
2019-08-23 08:23:37 -0700

    Update for Opera addon

--- package_extension.sh
+++ package_extension.sh
@@ -16,9 +16,16 @@ if [ "$USERVERSION" != "$MANIFESTVERSION" ]; then
 fi
 
 cp site/style.css extension/options.css
+cp userscript_smaller.user.js site/
+
+echo Building Firefox extension
+
+zipcmd() {
+    zip -r "$1" LICENSE.txt manifest.json userscript.user.js resources/logo_48.png resources/logo_96.png extension -x "*~"
+}
 
 rm extension.xpi
-zip -r extension.xpi LICENSE.txt manifest.json userscript.user.js resources/logo_48.png resources/logo_96.png extension -x "*~"
+zipcmd extension.xpi
 
 FILES=$(unzip -l extension.xpi | awk '{print $4}' | awk 'BEGIN{x=0;y=0} /^----$/{x=1} {if (x==1) {x=2} else if (x==2) {print}}' | sed '/^ *$/d' | sort)
 
@@ -45,3 +52,37 @@ if [ ! -z "$DIFF" ]; then
     echo Wrong files
     exit 1
 fi
+
+
+echo Building chrome extension
+# This is based on http://web.archive.org/web/20180114090616/https://developer.chrome.com/extensions/crx#scripts
+
+name=maxurl
+crx="$name.crx"
+pub="$name.pub"
+sig="$name.sig"
+zip="$name.zip"
+key="$name.pem"
+
+rm $zip $pub $sig
+zipcmd $zip
+
+# signature
+openssl sha1 -sha1 -binary -sign "$key" < "$zip" > "$sig"
+
+# public key
+openssl rsa -pubout -outform DER < "$key" > "$pub" 2>/dev/null
+
+byte_swap () {
+  # Take "abcdefgh" and return it as "ghefcdab"
+  echo "${1:6:2}${1:4:2}${1:2:2}${1:0:2}"
+}
+
+crmagic_hex="4372 3234" # Cr24
+version_hex="0200 0000" # 2
+pub_len_hex=$(byte_swap $(printf '%08x\n' $(ls -l "$pub" | awk '{print $5}')))
+sig_len_hex=$(byte_swap $(printf '%08x\n' $(ls -l "$sig" | awk '{print $5}')))
+(
+  echo "$crmagic_hex $version_hex $pub_len_hex $sig_len_hex" | xxd -r -p
+  cat "$pub" "$sig" "$zip"
+) > "$crx"
