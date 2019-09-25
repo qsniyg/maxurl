865ead8bace4e6e8528520703baec1301b486bea
qsniyg <qsniyg@mail.com>
2019-09-25 10:46:41 -0700

    Add license to about.js (hopefully fixing LibreJS)

--- about.js
+++ about.js
@@ -1,3 +1,19 @@
+/*
+	Copyright 2019 qsniyg
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 "use strict";
 
 var is_node = false;
@@ -33,7 +49,8 @@ function get_userscript_stats(response) {
     .replace(/\/\/ *-- *end *bigimage *--[\s\S]*$/, "");
   return [
     // rules
-    userscript_rcontents.match(/\n {8}(?:[/][*])?if /g).length,
+    //userscript_rcontents.match(/\n {8}(?:[/][*])?if /g).length,
+    userscript_rcontents.match(/\n\t{2}(?:[/][*])?if /g).length,
     // sites
     get_sites().length
     //userscript_rcontents.match(/[^/](?:domain(?:_[a-z]+)?|amazon_container|googlestorage_container) *===/g).length
