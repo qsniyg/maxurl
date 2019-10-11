1c3c44c371c732991732b095295688c3bd206ade
qsniyg <qsniyg@mail.com>
2019-10-11 13:14:34 -0700

    Use commit hash for logo

--- userscript.user.js
+++ userscript.user.js
@@ -9,7 +9,7 @@
 // @author       qsniyg
 // @homepageURL  https://qsniyg.github.io/maxurl/options.html
 // @supportURL   https://github.com/qsniyg/maxurl/issues
-// @icon         https://raw.githubusercontent.com/qsniyg/maxurl/master/resources/logo_256.png
+// @icon         https://raw.githubusercontent.com/qsniyg/maxurl/b5c5488ec05e6e2398d4e0d6e32f1bbad115f6d2/resources/logo_256.png
 // @include      *
 // @grant        GM.xmlHttpRequest
 // @grant        GM_xmlhttpRequest
