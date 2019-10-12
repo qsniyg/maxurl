f8efeda916fc8013871e7c790bfbf0f70666da9b
qsniyg <qsniyg@mail.com>
2019-10-12 14:55:26 -0700

    Don't update_waiting if !waiting_el

--- userscript.user.js
+++ userscript.user.js
@@ -46364,6 +46364,9 @@ var $$IMU_EXPORT$$;
 		};
 
 		function update_waiting() {
+			if (!waitingel)
+				return;
+
 			var x = mouseX;//mouseAbsX;
 			var y = mouseY;//mouseAbsY;
 			waitingel.style.left = (x - (waitingsize / 2)) + "px";
