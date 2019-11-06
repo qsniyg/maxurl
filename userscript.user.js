07d4df1c1238e42a3f264790315854f14dee12ab
qsniyg <qsniyg@mail.com>
2019-11-06 10:25:12 -0800

    0.11.10

--- userscript.user.js
+++ userscript.user.js
@@ -4,7 +4,7 @@
 // ==UserScript==
 // @name         Image Max URL
 // @namespace    http://tampermonkey.net/
-// @version      0.11.9
+// @version      0.11.10
 // @description  Finds larger or original versions of images for 5800+ websites
 // @author       qsniyg
 // @homepageURL  https://qsniyg.github.io/maxurl/options.html
