fccb530f5765034f91772f637a2b3780ebd63b0e
qsniyg <qsniyg@mail.com>
2019-12-18 20:13:54 -0800

    Print URLs the bot is requesting

--- reddit-bot/bot.js
+++ reddit-bot/bot.js
@@ -514,6 +514,8 @@ function dourl(url, post, options) {
         }
       }
 
+      console.log("Requesting ", options.url);
+
       var requestopts = {
         method: options.method,
         uri: options.url,
@@ -613,6 +615,8 @@ const links = new NodeCache({ stdTTL: 600, checkperiod: 100 });
 //dourl("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/55843f0c-f773-4b11-b8b1-89ed55c0b243/ddi92x8-76c983c5-4968-4058-be5e-a8442c4f69a8.png/v1/fill/w_1063,h_752,q_70,strp/colt_x_bo_nsfw_by_inflamedurethra_ddi92x8-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTA1IiwicGF0aCI6IlwvZlwvNTU4NDNmMGMtZjc3My00YjExLWI4YjEtODllZDU1YzBiMjQzXC9kZGk5Mng4LTc2Yzk4M2M1LTQ5NjgtNDA1OC1iZTVlLWE4NDQyYzRmNjlhOC5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.LeeiL-h6ir-ZMDO91siBlegbFy0OYq-DXFlvRecBQlY");
 // bad_if:
 //dourl("https://thumbs.ebaystatic.com/d/l225/pict/400793189705_4.jpg");
+// imgur nsfw that should still return a larger image
+//dourl("https://i.imgur.com/L4BmEfg_d.jpg?maxwidth=640&shape=thumb&fidelity=medium");
 
 //console.dir(blacklist_json.disallowed);
 if (true) {
