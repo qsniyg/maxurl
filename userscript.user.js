1cc05c77800a664d9aac17746666fe98532312bd
qsniyg <qsniyg@mail.com>
2019-12-24 04:18:16 -0800

    Increase UI opacity to 80%

--- userscript.user.js
+++ userscript.user.js
@@ -988,7 +988,7 @@ var $$IMU_EXPORT$$;
 		mouseover_video_loop: true,
 		mouseover_video_muted: false,
 		mouseover_ui: true,
-		mouseover_ui_opacity: 70,
+		mouseover_ui_opacity: 80,
 		mouseover_ui_imagesize: true,
 		mouseover_ui_zoomlevel: true,
 		mouseover_ui_gallerycounter: true,
