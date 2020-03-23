3212b0901bfe8117673c0c84e69b6524d059ef65
qsniyg <qsniyg@mail.com>
2020-03-23 02:34:39 -0700

    6400+

--- userscript.user.js
+++ userscript.user.js
@@ -5,14 +5,14 @@
 // @name              Image Max URL
 // @namespace         http://tampermonkey.net/
 // @version           0.12.19
-// @description       Finds larger or original versions of images for 6300+ websites, including a powerful image popup feature
-// @description:ko    6300개 이상의 사이트에 대해 고화질이나 원본 이미지를 찾아드립니다
-// @description:fr    Trouve des images plus grandes ou originales pour plus de 6300 sites
-// @description:es    Encuentra imágenes más grandes y originales para más de 6300 sitios
-// @description:zh    为6300多个网站查找更大或原始图像
-// @description:zh-CN 为6300多个网站查找更大或原始图像
-// @description:zh-TW 為6300多個網站查找更大或原始圖像
-// @description:zh-HK 為6300多個網站查找更大或原始圖像
+// @description       Finds larger or original versions of images for 6400+ websites, including a powerful image popup feature
+// @description:ko    6400개 이상의 사이트에 대해 고화질이나 원본 이미지를 찾아드립니다
+// @description:fr    Trouve des images plus grandes ou originales pour plus de 6400 sites
+// @description:es    Encuentra imágenes más grandes y originales para más de 6400 sitios
+// @description:zh    为6400多个网站查找更大或原始图像
+// @description:zh-CN 为6400多个网站查找更大或原始图像
+// @description:zh-TW 為6400多個網站查找更大或原始圖像
+// @description:zh-HK 為6400多個網站查找更大或原始圖像
 // @author            qsniyg
 // @homepageURL       https://qsniyg.github.io/maxurl/options.html
 // @supportURL        https://github.com/qsniyg/maxurl/issues
