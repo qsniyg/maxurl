9e4ea017a9802bc40b7033cafc09ec9d369e5472
qsniyg <qsniyg@mail.com>
2020-04-22 22:20:46 -0700

    Add service bot (in-progress)

---
+++ reddit-bot/service-bot.js
@@ -0,0 +1,274 @@
+const NodeCache = require("node-cache");
+var fs = require("fs");
+var yaml = require("js-yaml");
+
+var blacklist_json = JSON.parse(fs.readFileSync("./blacklist.json"));
+var env_json = {};
+
+require('dotenv').config({ path: "./.env-common" });
+require('dotenv').config({ path: "./.env-service" });
+env_json.user_agent = process.env.USERAGENT;
+env_json.client_id = process.env.CLIENT_ID;
+env_json.client_secret = process.env.CLIENT_SECRET;
+env_json.refresh_token = process.env.REFRESH_TOKEN;
+env_json.access_token = process.env.ACCESS_TOKEN;
+env_json.imgur_ua = process.env.IMGUR_UA;
+env_json.imgur_cookie = process.env.IMGUR_COOKIE;
+//env_json.username = process.env.REDDIT_USER;
+//env_json.password = process.env.REDDIT_PASS;
+
+//console.dir(env_json);
+
+const Snoowrap = require('snoowrap');
+const Snoostorm = require('snoostorm');
+
+const r = new Snoowrap(env_json);
+
+r.config({ requestDelay: 1001 });
+const client = new Snoostorm(r);
+
+var dourl = require("./dourl.js");
+
+const links = new NodeCache({ stdTTL: 600, checkperiod: 100 });
+
+function is_number(n) {
+	if (typeof n === "number")
+		return true;
+
+	if (typeof n === "string" && /^(?:[0-9]+|[0-9]*\.[0-9]+)$/.test(n)) {
+		return true;
+	}
+
+	return false;
+}
+
+function strip_whitespace(x) {
+	return x.replace(/^\s*|\s*$/g, "");
+}
+
+function parse_wiki_doc(doc, options) {
+	if (!doc || typeof doc !== "object") {
+		//console.log("Invalid YAML document");
+		return;
+	}
+
+	var options_map = {
+		"removable": true,
+		"lock_comment": true,
+		"sticky_comment": true,
+		"distinguish_comment": true,
+		"remove_post": true,
+		"explain_original": true,
+		"original_page": true,
+		//"blacklisted_words": "blacklist",
+		//"blacklisted_users": "blacklist",
+		"only_original": true,
+		"allow_nsfw": true,
+		"add_about_link": true,
+		"add_imu_links": true
+		//"min_ratio": true,
+		//"min_pixels": "thresh_px"
+	};
+
+	for (var option in options_map) {
+		if (option in doc) {
+			options[option] = !!doc[option];
+		}
+	}
+
+	if ("blacklisted_words" in doc && Array.isArray(doc.blacklisted_words)) {
+		if (!("blacklist" in options)) {
+			options.blacklist = [];
+		}
+
+		doc.blacklisted_words.forEach(function(word) {
+			if (typeof word === "string") {
+				options.blacklist.push(strip_whitespace(word.toLowerCase()));
+			}
+		});
+	}
+
+	if ("blacklisted_users" in doc && Array.isArray(doc.blacklisted_users)) {
+		if (!("user_blacklist" in options)) {
+			options.user_blacklist = [];
+		}
+
+		doc.blacklisted_users.forEach(function(user) {
+			if (typeof user === "string") {
+				options.user_blacklist.push(strip_whitespace(user.toLowerCase()));
+			}
+		});
+	}
+
+	if ("min_ratio" in doc && is_number(doc.min_ratio)) {
+		options.min_ratio = parseFloat(doc.min_ratio);
+	}
+
+	if ("min_pixels" in doc && is_number(doc.min_pixels)) {
+		options.thresh_px = parseInt(doc.min_pixels);
+	}
+}
+
+var default_options = {};
+
+function get_options_for_wikitext(wikitext) {
+	if (wikitext.length > 10*1024) {
+		console.log("Wiki text is too big!");
+		return null;
+	}
+
+	var options = {};
+
+	try {
+		yaml.safeLoadAll(wikitext, function(doc) {
+			parse_wiki_doc(doc, options);
+		}, {json: true});
+	} catch (e) {
+		console.error(e);
+	}
+
+	return options;
+}
+
+var wikioptions = new NodeCache({ stdTTL: 20, checkperiod: 60 });
+
+function get_wikitext_for_subreddit(subreddit, cb) {
+	r.getSubreddit(subreddit).getWikiPage("maximagebot").fetch().then(
+		wikipage => {
+			cb(wikipage.content_md);
+		},
+		error => {
+			//console.error(error);
+			//console.error("Unable to fetch maximagebot wiki page for ", subreddit);
+			cb(null);
+		}
+	);
+}
+
+function get_wikioptions_for_subreddit(subreddit, cb) {
+	subreddit = subreddit.toLowerCase();
+
+	var options = wikioptions.get(subreddit);
+	if (options) {
+		cb(options);
+	} else {
+		get_wikitext_for_subreddit(subreddit, function(text) {
+			if (!text) {
+				return cb({});
+			}
+
+			options = get_options_for_wikitext(text);
+			if (options) {
+				wikioptions.set(subreddit, options);
+			}
+
+			cb(options);
+		});
+	}
+}
+
+//var lastchecked = Date.now();
+
+var whitelisted_subreddits = [
+	"maximagetest"
+];
+//console.dir(blacklist_json.disallowed);
+if (true) {
+	var submissionStream = client.SubmissionStream({
+		"subreddit": "mod",
+		"results": 100,
+		// using a polltime of 1010 results in ratelimits
+		"pollTime": 10*1000
+	});
+
+	setInterval(() => {
+		r.getInbox({ "filter": "messages" }).then((inbox) => {
+			inbox.forEach((message_data) => {
+				if (!message_data["new"])
+					return;
+
+				var subreddit = message_data.subreddit_name_prefixed;
+
+				if (/^invitation to moderate \/?(?:[ru]|user)\/\S+$/.test(message_data.subject)) {
+					console.log("Invited to moderate " + subreddit);
+
+					try {
+						message_data.markAsRead();
+						r.getSubreddit(subreddit).acceptModeratorInvite();
+					} catch (e) {
+						console.error(e);
+					}
+				}
+			});
+		});
+	}, 10 * 1000);
+
+	submissionStream.on("submission", function (post) {
+		if (!post || !post.subreddit || !post.subreddit.display_name) {
+			console.error("Post is invalid", post);
+			return;
+		}
+
+		// for testing purposes
+		var post_subreddit = post.subreddit.display_name.toLowerCase();
+		if (whitelisted_subreddits.indexOf(post_subreddit) < 0)
+			return;
+
+		if (post.domain.startsWith("self.")) {
+			return;
+		}
+
+		var current_time = Date.now();
+		if ((current_time - (post.created_utc * 1000)) > 60*1000) {
+			console.log("Post is too old", post.permalink, current_time, post.created_utc);
+			return;
+		}
+
+		var options = {
+			imgur_ua: env_json.imgur_ua,
+			imgur_cookie: env_json.imgur_cookie,
+			exclude_mod: false,
+			blacklist: []
+		};
+
+		// service bot, so this section is useless
+		if (false && post.subreddit.display_name) {
+			if (blacklist_json.disallowed.indexOf(post.subreddit.display_name.toLowerCase()) >= 0 ||
+				blacklist_json.users.indexOf(post.author.name.toLowerCase()) >= 0) {
+				console.log(post.subreddit);
+				return;
+			}
+
+			if (blacklist_json.shocking.indexOf(post.subreddit.display_name.toLowerCase()) >= 0) {
+				options.shocking = true;
+			}
+
+			if (blacklist_json.np.indexOf(post.subreddit.display_name.toLowerCase()) >= 0) {
+				options.np = true;
+			}
+		}
+
+		if (links.get(post.permalink) === true) {
+			//console.log("Already processed " + post.permalink + ", skipping");
+			return;
+		}
+
+		links.set(post.permalink, true);
+
+		var url = post.url;
+
+		get_wikioptions_for_subreddit(post_subreddit, function(new_options) {
+			for (var option in new_options) {
+				options[option] = new_options[option];
+			}
+
+			console.log(options);
+
+			try {
+				dourl(url, post, options);
+			} catch (e) {
+				console.error(e);
+			}
+		});
+	});
+}
