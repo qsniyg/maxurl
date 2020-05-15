9c266e3b05654e3b6da3ff9f3a55bee061c27156
qsniyg <qsniyg@mail.com>
2020-05-15 11:54:30 -0700

    Support finding original page/caption for dreamstime

--- userscript.user.js
+++ userscript.user.js
@@ -60075,6 +60075,69 @@ var $$IMU_EXPORT$$;
 			}
 		}
 
+		if (domain === "thumbs.dreamstime.com") {
+			// https://thumbs.dreamstime.com/z/%D0%BD%D1%8C%D1%8E-%D0%B9%D0%BE%D1%80%D0%BA-%D0%BE%D0%B5-%D1%81%D0%B5%D0%BD%D1%82%D1%8F%D0%B1%D1%80%D1%8F-%D0%BC%D0%BE-%D0%B5-%D1%8C%D0%BD%D1%8B%D0%B9-alessandra-ambrosio-%D0%BF%D1%80%D0%B5-%D1%81%D1%82%D0%B0%D0%B2-%D1%8F%D0%B5%D1%82-%D0%BA%D1%83-%D1%83%D0%B0%D1%80%D0%BD%D0%BE%D0%B5-30755744.jpg
+			//   https://www.dreamstime.com/stock-images-new-york-september-model-alessandra-ambrosio-poses-backstage-cipriani-restaurant-rock-republic-spring-summer-image30755744
+			match = src.match(/^[a-z]+:\/\/[^/]+\/+[a-z]\/+[^/]+-([0-9]+)\.[^/.]+(?:[?#].*)?$/);
+			if (match) {
+				id = match[1];
+
+				var baseobj = {
+					url: src,
+					extra: {
+						page: "https://www.dreamstime.com/" + id
+					}
+				};
+
+				if (options.do_request && options.cb) {
+					var cache_key = "dreamstime: " + id;
+
+					api_cache.fetch(cache_key, function(data) {
+						if (!data) {
+							return options.cb(baseobj);
+						} else {
+							return options.cb({
+								url: src,
+								extra: data
+							});
+						}
+					}, function(done) {
+						options.do_request({
+							url: "https://www.dreamstime.com/" + id,
+							method: "GET",
+							onload: function(resp) {
+								if (resp.status !== 200) {
+									return done(null, false);
+								}
+
+								var extra = {
+									page: resp.finalUrl
+								};
+
+								var match = resp.responseText.match(/<script type="application\/ld\+json">({.*?})<\/script>/);
+								if (match) {
+									try {
+										var json = JSON_parse(match[1]);
+										extra.caption = json.name;
+									} catch (e) {
+										console_error(cache_key, e);
+									}
+								}
+
+								done(extra, 24*60*60);
+							}
+						});
+					});
+
+					return {
+						waiting: true
+					};
+				}
+
+				return baseobj;
+			}
+		}
+
 
 
 
