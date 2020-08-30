74e12cae3f5f4996330413689ccad92980ee8008
qsniyg <qsniyg@mail.com>
2020-08-30 04:03:35 -0700

    Fix #410

--- userscript.user.js
+++ userscript.user.js
@@ -88299,6 +88299,12 @@ var $$IMU_EXPORT$$;
 			var map = get_keystrs_map(e, true);
 
 			for (var key in map) {
+				// otherwise, modifiers like ctrl etc. get counted, even if they're not pressed (because get_keystrs_map reports them as false)
+				// todo: if this function is ever called in keyup, another solution is needed.
+				// for now, it'll only ever be called on keydown
+				if (!map[key])
+					continue;
+
 				if (keystr_in_trigger(key, wanted_chord))
 					return true;
 			}
