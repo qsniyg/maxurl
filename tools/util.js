582c47df05c8612aaa3fca32acdd503b963c17e7
qsniyg <qsniyg@mail.com>
2020-09-01 13:35:41 -0700

    Update tools/util.js

--- tools/util.js
+++ tools/util.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 // https://stackoverflow.com/a/31652607/13255485
 var json_escape_unicode = function(stringified) {
     return stringified.replace(/[\u007F-\uFFFF]/g, function(chr) {
@@ -5,3 +7,26 @@ var json_escape_unicode = function(stringified) {
 	});
 };
 module.exports.json_escape_unicode = json_escape_unicode;
+
+var strings_regex = module.exports.strings_regex = /(\n\tvar strings = )({[\s\S]+?})(;\n)/;
+
+var stringify_strings = function(strings) {
+    var stringified = JSON.stringify(strings, null, "\t").replace(/\n/g, "\n\t");
+    stringified = json_escape_unicode(stringified);
+
+    return stringified;
+};
+module.exports.stringify_strings = stringify_strings;
+
+module.exports.update_userscript_strings = function(strings, userscript) {
+    var stringified = stringify_strings(strings);
+
+    var userscript_filename = __dirname + "/../userscript.user.js";
+
+    if (!userscript)
+        userscript = fs.readFileSync(userscript_filename).toString();
+
+    userscript = userscript.replace(strings_regex, "$1" + stringified + "$3");
+
+    fs.writeFileSync(userscript_filename, userscript);
+};
