ae32f343fc2e8cd6a45860b49ad86ce3f0b3bbe8
qsniyg <qsniyg@mail.com>
2021-04-08 17:42:26 -0700

    Add minified userscript

--- tools/gen_minified.js
+++ tools/gen_minified.js
@@ -18,16 +18,8 @@ var get_uglifyjs_version = function(cb) {
 	});
 };
 
-console.log("Minifying...");
-var prc = spawn("uglifyjs", ['-m', '-c', '-o', 'userscript_min.user.js', '--', 'userscript.user.js']);
-prc.on('close', function(code) {
-	console.log("Finished minifying");
-	if (code !== 0) {
-		console.log("Wrong status code:", code);
-		return;
-	}
-
-	var userscript = fs.readFileSync(process.argv[2] || "userscript.user.js").toString();
+var read_userscript_header = function(path) {
+	var userscript = fs.readFileSync(path).toString();
 	var lines = userscript.split("\n");
 
 	var header = null;
@@ -55,7 +47,24 @@ prc.on('close', function(code) {
 		}
 	}
 
-	fs.writeFileSync("userscript.meta.js", header);
+	return header;
+};
+
+var in_filename = "build/userscript_extr.user.js";
+var out_filename = "build/userscript_extr_min.user.js";
+var reserved = ["$__imu_get_bigimage"];
+
+console.log("Minifying...");
+var prc = spawn("uglifyjs", ['-m', 'reserved=' + JSON.stringify(reserved), '-c', '-o', out_filename, '--', in_filename], {stdio: "inherit"});
+prc.on('close', function(code) {
+	console.log("Finished minifying");
+	if (code !== 0) {
+		console.log("Wrong status code:", code);
+		return;
+	}
+
+	var userscript_header = read_userscript_header(process.argv[2] || "userscript.user.js");
+	fs.writeFileSync("userscript.meta.js", userscript_header);
 
 	get_uglifyjs_version(function(version) {
 		if (!version) {
@@ -65,15 +74,17 @@ prc.on('close', function(code) {
 			version = "v" + version;
 		}
 
-		header += "// Due to OpenUserJS's 1MB limit, the source code had to be minified.\n";
-		header += "// The minification was done by gen_minified.js (in the maxurl repository below) using `uglifyjs -m -c` (" + version + ").\n";
-		header += "// This unfortunately renders the code pretty much unreadable, but it was the only way to fit it within the 1MB limit.\n";
-		header += "// You can view the original source code here: https://github.com/qsniyg/maxurl/blob/master/userscript.user.js\n";
-		header += "// Please let me know if you have any questions or concerns regarding the script.\n";
-		header += "\n";
+		var extr_header = read_userscript_header(in_filename);
 
-		var min_userscript = fs.readFileSync("userscript_min.user.js").toString();
+		extr_header += "// Due to OpenUserJS's 1MB limit, the source code had to be minified.\n";
+		extr_header += "// The minification was done by gen_minified.js (in the maxurl repository below) using `uglifyjs -m -c` (" + version + ").\n";
+		extr_header += "// This unfortunately renders the code pretty much unreadable, but it was the only way to fit it within the 1MB limit.\n";
+		extr_header += "// You can view the original source code here: https://github.com/qsniyg/maxurl/blob/master/userscript.user.js\n";
+		extr_header += "// Please let me know if you have any questions or concerns regarding the script.\n";
+		extr_header += "\n";
 
-		fs.writeFileSync("userscript_min.user.js", header + min_userscript);
+		var min_userscript = fs.readFileSync(out_filename).toString();
+
+		fs.writeFileSync(out_filename, extr_header + min_userscript);
 	});
 });
