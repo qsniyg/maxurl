64a7ffeb46cbbe8fae8db410d19a915e574b8e4c
qsniyg <qsniyg@mail.com>
2021-08-04 15:09:26 -0700

    More JS->TS

--- src/module.d.ts
+++ src/module.d.ts
@@ -13,21 +13,44 @@ declare function GM_setValue(key:string, value:any):void;
 declare function GM_setClipboard(data:string, options?:any):void;
 declare function GM_addValueChangeListener(key:string, cb:Function):void;
 declare function GM_notification(details:Object, ondone:Function):void;
+declare function GM_xmlhttpRequest(data:Object):void;
+declare var GM_info: (Object|Function);
+declare var GM_fetch: Function;
+declare var GM_registerMenuCommand: Function;
+declare var GM_unregisterMenuCommand: Function;
+declare var GM_openInTab: Function;
+declare var GM_download: Function;
 
 declare var GM: {
 	getValue: (key:string, default_value?:any)=>Promise<any>;
 	setValue: (key:string, value:any)=>Promise;
 	setClipboard: (data:string)=>void,
-	notification: (details:Object, ondone:Function)=>void
+	notification: (details:Object, ondone:Function)=>void,
+	xmlHttpRequest: (data:Object)=>void,
+	openInTab: Function
 }
 
 declare function require(filename:string):any;
+declare var Buffer: {
+	from: (...args)=>any;
+};
 declare var module: { exports: any };
+
 declare var imu_variable:any;
 declare var imu_xhr:{ custom_xhr: class }; // for custom_xhr_wrap, which is stringified
 
 declare var chrome: {
 	permissions: {
 		request: Function
+	},
+	runtime: {
+		getURL: (url:string)=>any;
+		getManifest: ()=>any;
+		onMessage: {
+			addListener: Function
+		}
 	}
 }
+
+declare var userscript_extension_message_handler:Function;
+declare var imu_userscript_message_sender:Function;
