52908a18110181edfcd745fc61b0780c44213826
qsniyg <qsniyg@mail.com>
2022-01-01 05:35:52 +0000

    Support unicode characters in download filenames for extension

--- extension/background.js
+++ extension/background.js
@@ -632,11 +632,14 @@ function stringify_contentdisposition(cdp) {
 				quotec = "'";
 			}
 
-			if (!cdp[i][1].match(/\s/g)) {
+			// content-disposition should always be quoted
+			if (false && !cdp[i][1].match(/\s/g)) {
 				quotec = "";
 			}
 
-			out_strings.push(cdp[i][0] + "=" + quotec + cdp[i][1] + quotec);
+			// thanks to I Van on discord for reporting:
+			// http://blogfiles.naver.net/MjAxOTA0MDlfOTkg/MDAxNTU0NzkwNTQ3MTI5.b6JSzkFxWP05cZjSrHQmTukBQZXgdC3XQpYXO_5Z81wg.KSktht2QStZGSj4o4JcX4Adz8wJyG3c7KrrFXg7LwJYg.JPEG.dirtigerside/%EC%BC%80%EC%9D%B4001.jpg
+			out_strings.push(cdp[i][0] + "=" + quotec + encodeURIComponent(cdp[i][1]) + quotec);
 		} else {
 			out_strings.push(cdp[i][0]);
 		}
