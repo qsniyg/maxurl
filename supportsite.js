1ab51f6912969555b0b975a8c9d91ca55bb7e3b0
qsniyg <qsniyg@mail.com>
2023-06-30 21:40:47 +0000

    Add images.genius.com to the blacklist

--- supportsite.js
+++ supportsite.js
@@ -35,7 +35,8 @@ var reqsite_invalid_domains = [
 
 var reqsite_invalid_urls = [
     /^https?:\/\/i\.redd\.it\/+[0-9a-z]+\.[a-z]+$/,
-    /^https?:\/\/i\.scdn\.co\/+image\/+[0-9a-f]{40}$/
+    /^https?:\/\/i\.scdn\.co\/+image\/+[0-9a-f]{40}$/,
+    /^https?:\/\/images\.genius\.com\/+[0-9a-f]{32}\.[0-9]+x[0-9]+x[0-9]+\.[a-z]+$/
 ];
 
 function reqsite_valid_url(url) {
