85042e125c2f270063d535537c73ffe27b54c814
qsniyg <qsniyg@mail.com>
2023-11-16 23:21:46 +0000

    Sort git tag by version number for get_old_userscript

--- tools/get_old_userscript.sh
+++ tools/get_old_userscript.sh
@@ -2,5 +2,5 @@
 
 cd "`dirname "$0"`/.."
 
-LASTTAG="`git tag | tail -n1`"
+LASTTAG="`git tag | sort -V | tail -n1`"
 curl 'https://raw.githubusercontent.com/qsniyg/maxurl/'"$LASTTAG"'/userscript_smaller.user.js' -o olduserscript
