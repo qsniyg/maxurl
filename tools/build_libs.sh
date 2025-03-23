5d8a153e7ccb2272cc1162422ece252c2666aab0
qsniyg <qsniyg@mail.com>
2025-03-23 21:41:15 +0000

    Update built ffmpeg library

--- tools/build_libs.sh
+++ tools/build_libs.sh
@@ -29,45 +29,57 @@ node ../tools/patch_libs.js acorn_interpreter orig > acorn_interpreter.js
 if [ -f orig/ffmpeg.min.js ]; then
 	cp orig/ffmpeg.min.js ffmpeg.min.orig.js
 	cp orig/ffmpeg-core.js ffmpeg-core.orig.js
+	cp orig/ffmpeg-core.worker.js ffmpeg-core.worker.js
 	cp ffmpeg-core.orig.js ffmpeg-core.js
 	# window.* and self->_fakeGlobal are for preventing leaking
 	# remove sourcemapping to avoid warnings under devtools
 	sed -i \
 		-e 's/window.FFMPEG_CORE_WORKER_SCRIPT/FFMPEG_CORE_WORKER_SCRIPT/g' \
 		-e 's/window\.createFFmpegCore/createFFmpegCore/g' \
-		-e 's/(self,(function/(_fakeGlobal,(function/' \
+		-e 's/(self,(()=>/(_fakeGlobal,(()=>/' \
 		-e '/\/\/# sourceMappingURL=/d' ffmpeg.min.orig.js
+	# since ffmpeg-core is being prepended, this is necessary in order to have requests work properly
+	# note that the unpkg url is used instead of integrating it in the repo. this is for cache reasons, as all other scripts using ffmpeg.js will use the same url
+	sed -i 's/{return [a-z]*\.locateFile[?][a-z]*\.locateFile(a,[^}]*}var/{return "https:\/\/unpkg.com\/@ffmpeg\/core@0.12.9\/dist\/" + a}var/' ffmpeg-core.js
 	# prevents blob urls from being used, fixes loading under chrome
 	# node is used instead of sed due to the size of ffmpeg-core.orig.js
 	node <<EOF
 var fs = require("fs");
 var ffmpeg = fs.readFileSync("ffmpeg.min.orig.js", "utf8");
-var core = fs.readFileSync("ffmpeg-core.orig.js", "utf8");
+var core = fs.readFileSync("ffmpeg-core.js", "utf8");
+var worker = fs.readFileSync("ffmpeg-core.worker.js", "utf8");
 //ffmpeg = ffmpeg.replace(/mainScriptUrlOrBlob:[a-zA-Z0-9]+,/, 'mainScriptUrlOrBlob:"data:application/x-javascript,' + encodeURIComponent(core) + '",');
-ffmpeg = ffmpeg.replace(/mainScriptUrlOrBlob:[a-zA-Z0-9]+,/, 'mainScriptUrlOrBlob:new Blob([decodeURIComponent("' + encodeURIComponent(core) + '")]),');
+//ffmpeg = ffmpeg.replace(/mainScriptUrlOrBlob:[a-zA-Z0-9]+,/, 'mainScriptUrlOrBlob:new Blob([decodeURIComponent("' + encodeURIComponent(core) + '")]),');
+//fs.writeFileSync("ffmpeg.min.orig.js", ffmpeg);
+worker = worker.replace(/"https:\/\/unpkg.com\/@ffmpeg\/core@[^/]+\/dist\/umd\/ffmpeg-core.js"/, 'URL.createObjectURL(new Blob([decodeURIComponent("' + encodeURIComponent(core) + '")]))');
+fs.writeFileSync("ffmpeg-core.worker.js", worker);
+
+ffmpeg = ffmpeg.replace(/new Worker\(new URL\([a-z.]*[+][a-z.]*\([0-9]*\),[a-z.]*\),{/, 'new Worker(URL.createObjectURL(new Blob([decodeURIComponent("' + encodeURIComponent(worker) + '")])),{');
 fs.writeFileSync("ffmpeg.min.orig.js", ffmpeg);
 EOF
-	# since ffmpeg-core is being prepended, this is necessary in order to have requests work properly
-	# note that the unpkg url is used instead of integrating it in the repo. this is for cache reasons, as all other scripts using ffmpeg.js will use the same url
-	sed -i 's/{return [a-z]*\.locateFile[?][a-z]*\.locateFile(a,[^}]*}var/{return "https:\/\/unpkg.com\/@ffmpeg\/core@0.8.5\/dist\/" + a}var/' ffmpeg-core.js
+	#CORE_CODE=`to_uricomponent ffmpeg-core.js`
+	#sed -i 's/"https:\/\/unpkg.com\/@ffmpeg\/core@[0-9.]*\/dist\/umd\/ffmpeg-core.js"/URL.createObjectURL(new Blob([decodeURIComponent("'$CORE_CODE'")]))/g' ffmpeg-core.worker.js
 	# inject the worker directly, fixes more cors issues
-	WORKER_CODE=`to_uricomponent orig/ffmpeg-core.worker.js`
+	WORKER_CODE=`to_uricomponent ffmpeg-core.worker.js`
 	#sed -i 's/{var a=..("ffmpeg-core.worker.js");\([^}]*\.push(new Worker(a))}\)/{var a="data:application\/x-javascript,'$WORKER_CODE'";\1/g' ffmpeg-core.js
 	# use blob instead of data, works on more sites (such as instagram)
-	sed -i 's/{var a=..("ffmpeg-core.worker.js");\([^}]*\.push(new Worker(a))}\)/{var a=URL.createObjectURL(new Blob([decodeURIComponent("'$WORKER_CODE'")]));\1/g' ffmpeg-core.js
+	#sed -i 's/{var a=..("ffmpeg-core.worker.js");\([^}]*\.push(new Worker(a))}\)/{var a=URL.createObjectURL(new Blob([decodeURIComponent("'$WORKER_CODE'")]));\1/g' ffmpeg-core.js
+	#sed -i 's/new Worker(new URL([a-z.]*[+][a-z.]*([0-9]*),[a-z.]*),{/new Worker(URL.createObjectURL(new Blob([decodeURIComponent("'$WORKER_CODE'")])),{/g' ffmpeg.min.orig.js
+	# we're patching out the path, so no need for this error
+	sed -i 's/(!\([a-z]\)) *throw new Error("Automatic publicPath is not supported in this browser");/(!\1){\1=""}/' ffmpeg.min.orig.js
 	# finally cat it all together
 	echo "var FFMPEG_CORE_WORKER_SCRIPT;var _fakeGlobal={window:window};" > ffmpeg.js
 	cat fetch_shim.js >> ffmpeg.js
-	cat ffmpeg-core.js >> ffmpeg.js
+	#cat ffmpeg-core.js >> ffmpeg.js
 	echo "" >> ffmpeg.js
 	cat ffmpeg.min.orig.js >> ffmpeg.js
 	echo "" >> ffmpeg.js
-	echo "var lib_export = _fakeGlobal.FFmpeg;" >> ffmpeg.js
+	echo "var lib_export = _fakeGlobal.FFmpegWASM;" >> ffmpeg.js
 	cat shim.js >> ffmpeg.js
 	dos2unix ffmpeg.js
 	strip_whitespace ffmpeg.js
 
-	CLEANUP_FILES="$CLEANUP_FILES ffmpeg.min.orig.js ffmpeg-core.orig.js ffmpeg-core.js"
+	CLEANUP_FILES="$CLEANUP_FILES ffmpeg.min.orig.js ffmpeg-core.orig.js ffmpeg-core.js ffmpeg-core.worker.js"
 fi
 
 if [ -f orig/mpd-parser.js ]; then
