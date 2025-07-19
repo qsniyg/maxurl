062630b90b5048f9b83d2048041bf7758ef1bbbd
qsniyg <qsniyg@mail.com>
2025-07-19 14:04:32 +0000

    Better patch jszip against requirejs

--- tools/patch_libs.js
+++ tools/patch_libs.js
@@ -106,7 +106,7 @@ var patch_lib = null;
 		// don't store in window
 		// remove setImmediate delay, which causes zipping to be extremely slow
 		text = text
-			.replace(/^/, "var _fakeWindow={};\n")
+			.replace(/^/, "var _fakeWindow={};\nvar define = void 0;\nvar module = void 0;\n")
 			.replace(/\("undefined"!=typeof window.window:"undefined"!=typeof global.global:"undefined"!=typeof self.self:this\)/g, "(_fakeWindow)")
 			.replace(/\("undefined"!=typeof window.window:void 0!==...:"undefined"!=typeof self\?self:this\)/g, "(_fakeWindow)")
 			.replace(/if\(typeof window!=="undefined"\){g=window}/, 'if(typeof _fakeWindow!=="undefined"){g=_fakeWindow}')
